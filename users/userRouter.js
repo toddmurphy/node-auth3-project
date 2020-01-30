dd4136407fb1950b97214f95154dee41720fa78c
toddmurphy <toddmurphy93@gmail.com>
2020-01-29 22:03:15 -0400

    trying to get stretch working

--- users/userRouter.js
+++ users/userRouter.js
@@ -5,7 +5,7 @@ const restricted = require('../auth/restricted-middleware');
 const router = express.Router();
 
 //get (GET) --> endpoint beginning with api/users
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, onlyDepartment('jedi'), (req, res) => {
   Users.getUsers()
     .then(user => {
       console.log('inside getUsers', user);
@@ -16,5 +16,17 @@ router.get('/', restricted, (req, res) => {
       res.status(500).json({ message: 'Sorry, no users returned from the server', error });
     });
 });
+
+//only 'department' shows when getting users
+function onlyDepartment(department) {
+  return function(req, res, next) {
+    console.log(req.user.department);
+    if (req.user && req.user.department === department) {
+      next();
+    } else {
+      res.status(403).json({ message: 'This shows only departments' });
+    }
+  };
+}
 
 module.exports = router;
