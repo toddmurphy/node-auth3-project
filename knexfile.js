1ef64884c133987be1774c8f21539604c0042e99
toddmurphy <toddmurphy93@gmail.com>
2020-02-01 15:40:18 -0400

    add postgres

--- knexfile.js
+++ knexfile.js
@@ -25,5 +25,16 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
